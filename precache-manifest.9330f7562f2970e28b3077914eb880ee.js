757d2652e45ccd3d6bebffcfc9f8bcaa674d9c85
yassirDWM <benali.yassire@gmail.com>
2020-08-13 13:34:12 +0200

    Updates

---
+++ precache-manifest.9330f7562f2970e28b3077914eb880ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e64c07285df24f8cf12048af18ef93",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ad88b15f025c8f7ba41b",
+    "url": "/monsters-rolodex/static/css/main.3195a446.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad88b15f025c8f7ba41b",
+    "url": "/monsters-rolodex/static/js/main.ba07a53f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
